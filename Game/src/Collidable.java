ba860f653326ac7662aebdbbcf7fbc6dcde83e47
spazses <jack.yang1212>
2017-05-08 08:48:39 -0700

    i'm not ready for this kind of commitment

--- Game/src/Collidable.java
+++ Game/src/Collidable.java
@@ -1,38 +1,28 @@
 public class Collidable
 {
-	private int entWidth;
-	private int entLength;
-	
 	private int[] xCoords;
 	private int[] yCoords;
+	private final int hitboxPts = 5;
 	private int type;
 	
 	public Collidable(int xinit, int yinit, int type)
 	{
-		xCoords = new int[4];
-		yCoords = new int[4];
-		switch(type)
+		xCoords = new int[hitboxPts];
+		yCoords = new int[hitboxPts];
+	}
+	
+	public boolean compareTo(Collidable other)
+	{
+		for(int k = 0; k < hitboxPts; k++)
 		{
-			case 1:
-				xCoords[0] = xinit - entWidth / 2;
-				yCoords[0] = yinit - entLength / 2;
-				xCoords[1] = xinit + entWidth / 2;
-				yCoords[1] = yinit - entLength / 2;
-				xCoords[2] = xinit - entWidth / 2;
-				yCoords[2] = yinit + entLength / 2;
-				xCoords[3] = xinit + entWidth / 2;
-				yCoords[3] = yinit + entLength / 2;
-			break;
-			case 2:
-				xCoords[0] = xinit;
-				yCoords[0] = yinit - entLength / 2;
-				xCoords[1] = xinit + entWidth / 2;
-				yCoords[1] = yinit;
-				xCoords[2] = xinit - entWidth / 2;
-				yCoords[2] = yinit;
-				xCoords[3] = xinit;
-				yCoords[3] = yinit + entLength / 2;
-			break;
+			for(int j = 0; j < hitboxPts; j++)
+			{
+				if(xCoords[k] == other.xCoords[k] && yCoords[k] == other.yCoords[k])
+				{
+					return true;
+				}
+			}
 		}
+		return false;
 	}
 }
