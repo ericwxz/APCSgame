e423f34015afbdb53c494680237d42e834613bac
spazses <jack.yang1212>
2017-05-09 08:48:11 -0700

    dude

--- Game/src/Collidable.java
+++ Game/src/Collidable.java
@@ -1,57 +1,17 @@
 public class Collidable
 {
-	private int[] xCoords;
-	private int[] yCoords;
-	private final int hitboxPts = 7;
-	private int type;
 	
 	public Collidable(int xinit, int yinit, int type)
 	{
-		xCoords = new int[hitboxPts];
-		yCoords = new int[hitboxPts];
-		
-		switch(type)
-		{
-			case 1:
-				xCoords[0] = xinit; yCoords[0] = yinit;
-				xCoords[1] = xinit; yCoords[1] = yinit + 50;
-				xCoords[2] = xinit; yCoords[2] = yinit - 50;
-				xCoords[3] = xinit + 50; yCoords[3] = yinit;
-				xCoords[4] = xinit - 50; yCoords[4] = yinit;
-				xCoords[5] = xinit + 25; yCoords[5] = yinit;
-				xCoords[6] = xinit - 25; yCoords[6] = yinit;
-			break;
-			case 1:
-				xCoords[0] = xinit; yCoords[0] = yinit;
-				xCoords[1] = xinit; yCoords[1] = yinit + 50;
-				xCoords[2] = xinit; yCoords[2] = yinit - 50;
-				xCoords[3] = xinit + 50; yCoords[3] = yinit;
-				xCoords[4] = xinit - 50; yCoords[4] = yinit;
-				xCoords[5] = xinit + 25; yCoords[5] = yinit;
-				xCoords[6] = xinit - 25; yCoords[6] = yinit;
-			break;
-				
-		}
+
 	}
 	
 	public boolean compareTo(Collidable other)
 	{
-		for(int k = 0; k < hitboxPts; k++)
-		{
-			for(int j = 0; j < hitboxPts; j++)
-			{
-				if(xCoords[k] == other.xCoords[k] && yCoords[k] == other.yCoords[k])
-				{
-					return true;
-				}
-			}
-		}
 		return false;
 	}
 	
 	public void move(int x, int y)
 	{
-		xCoords[0] += x;
-		yCoords[0] += y;
 	}
 }
---
+++ Game/src/GUI.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GUI
+{
+	private JLabel label;
+	private JLayeredPane layers;
+	private World myWorld;
+
+	public GUI(World w){}
+
+	public void startGame(){}
+	public void initFrame(JFrame j){}
+	public void paint(ArrayList a){}
+	private void paintBuffer(ArrayList a){}
+	public void actionPerformed(ActionEvent e){}
+}
