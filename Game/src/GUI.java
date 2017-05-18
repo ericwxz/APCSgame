55ebe358c621b6b21ca356022e50d7bb2a79d515
1904660 <1904660@00-46211.pusd.dom>
2017-05-18 11:06:09 -0700

    :)(:

--- Game/src/GUI.java
+++ Game/src/GUI.java
@@ -17,6 +17,7 @@ public class GUI extends JFrame implements ActionListener
 	private Image bullet;
 	private Image bbullet;
 	private Image muzzflash;
+	private Image explo;
 
 	private int steps;
 	private boolean inMenu;
@@ -55,7 +56,7 @@ public class GUI extends JFrame implements ActionListener
 		bbullet = bbullety.getImage();
 		bbullet = bbullet.getScaledInstance(50,50,1);
 		
-		exploy = new ImageIcon("explosion (1).gif");
+		ImageIcon exploy = new ImageIcon("explosion (1).gif");
 		explo = exploy.getImage();
 		explo = explo.getScaledInstance(100,100,1);
 		
@@ -137,37 +138,51 @@ public class GUI extends JFrame implements ActionListener
 		myWorld.move(steps);
 		myWorld.act(steps);
 		myWorld.cleanBounds(steps);
-		if(steps % 50 == 0)
+/*		if(steps % 50 == 0)
 		{
 			System.out.println("it's been " + steps + " ticks");
 		}
 		repaint();
 		//detect which button is cliked in the menu
-//		if (start.isSelected())
-//		{
-//			planeLabel.requestFocusInWindow();
-//		}
-//		else if (help.isSelected())
-//		{
-//			Container directions = super.getContentPane();
-//			
-//			ImageIcon arrowIcon = new ImageIcon(".gif");
-//			arrowKeys = arrowIcon.getImage();
-//			arrowKeys = arrowKeys.getScaledInstance(100,100,1);
-//			
-//			ImageIcon spaceIcon = new ImageIcon(".gif");
-//			spaceKey = spaceIcon.getImage();
-//			spaceKey = spaceKey.getScaledInstance(100,100,1);
-//
-//			howToPlay = new JTextArea(100,50);
-//			directions.add(howToPlay);
-//			String playerHelp = "How To Play: " + "\n" + ""
-//			howToPlay.setText();
-//		}
-//		else
-//		{
-//			close everything
-//		}
+		if (start.isSelected())
+		{
+			planeLabel.requestFocusInWindow();
+		}
+		else if (help.isSelected())
+		{
+			Container directions = super.getContentPane();
+
+			howToPlay = new JTextArea(100,50);
+			
+			ImageIcon arrowIcon = new ImageIcon("i love clouds.gif");
+			arrowKeys = arrowIcon.getImage();
+			arrowKeys = arrowKeys.getScaledInstance(100,100,1);
+			
+			ImageIcon spaceIcon = new ImageIcon("i love clouds.gif");
+			spaceKey = spaceIcon.getImage();
+			spaceKey = spaceKey.getScaledInstance(100,100,1);
+
+			ImageIcon escapeIcon = new ImageIcon("i love clouds.gif");
+			escapeKey = escapeIcon.getImage();
+			escapeKey = escapeKey.getScaledInstance(100,100,1);
+
+			directions.add(howToPlay);
+
+			String playerHelp = "How To Play: " + "\n" + "Welcome to Aerial Ace! Your mission "
+								+ "is to defeat as many enemy planes as possible. You have 5 lives"
+								+ " initially, and every time you get hit by an enemy bullet, you"
+								+ " lose 1 life. If you collide with an enemy plane... ouch!" +
+								" That's 3 lives gone! When you are out of lives, your mission "
+								+ "must be aborted. Good luck, soldier! We have faith in you."
+								+ "\n" + "Use arrow keys to change your plane's position"
+							    + "\n" + "Press space to fire" + "\n" +	"Press Esc to exit";
+								
+			howToPlay.setText(playerHelp);
+		}
+		else
+		{
+			System.exit(0);
+		}*/
 	}
 	private class MenuStartListener implements ActionListener
 	{
